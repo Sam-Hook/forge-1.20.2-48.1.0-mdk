e2ea1e49c2878e34a1c3128181c567083244fc38
Sam Hook <sam.hook93@gmail.com>
2023-12-31 19:35:52 -0800

    Important Changes Made

--- src/main/java/net/samhook/tutorialmod/TutorialMod.java
+++ src/main/java/net/samhook/tutorialmod/TutorialMod.java
@@ -25,7 +25,6 @@ public class TutorialMod {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
-
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
     }
